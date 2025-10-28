95dcc593064cc50d74620b7414ffd8393a2da268
AnasH232 <AH2300408@tkh.edu.eg>
2025-10-28 11:25:00 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -1,14 +1,19 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int i = 1;
 
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
     if (n == 0) return;
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    towerOfHanoi(n - 1, from_rod, to_rod, aux_rod);
     cout << "Move no. " << i << ".. ";
-    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << aux_rod << endl;
     i++;
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    towerOfHanoi(n - 1, to_rod, from_rod, aux_rod);
+    cout << "Move no. " << i << ".. ";
+    cout << "Move disk " << n << " from rod " << aux_rod << " to rod " << to_rod << endl;
+    i++;
+    towerOfHanoi(n - 1, from_rod, to_rod, aux_rod);
 }
 
 int main() {
